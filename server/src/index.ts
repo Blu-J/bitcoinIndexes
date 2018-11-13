c248faa14747926472550dc034131abc550b29ef
Justin Miller <mogulslayer@gmail.com>
2018-11-12 21:18:58 -0700

    simplify server

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,16 +2,20 @@ import "reflect-metadata";
 import { GraphQLServer } from "graphql-yoga";
 import resolvers from "./resolvers";
 import schema from "./schema";
+import express = require("express");
 
 const server = new GraphQLServer({
   typeDefs: schema,
   resolvers: resolvers as any
-});
+} as any);
+server.express.use(express.static("server/out"));
+const port = process.env.PORT || 9000;
 server.start(
   {
-    endpoint: "/graphql"
+    endpoint: "/graphql",
+    port
   },
-  () => console.log("🚀 Server is running on localhost:4000")
+  () => console.log(`🚀 Server is running on localhost:${port}`)
 );
 
 export default server;
