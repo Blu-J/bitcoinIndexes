9f53140a350d85e88a5709a6b784d96fc06be42e
Justin Miller <mogulslayer@gmail.com>
2018-11-12 23:14:10 -0700

    feat: adding compress

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,11 +3,14 @@ import { GraphQLServer } from "graphql-yoga";
 import resolvers from "./resolvers";
 import schema from "./schema";
 import express = require("express");
+import compression from "compression";
 
 const server = new GraphQLServer({
   typeDefs: schema,
   resolvers: resolvers as any
 } as any);
+
+server.express.use(compression());
 server.express.use(express.static("out"));
 const port = process.env.PORT || 9000;
 server.start(
